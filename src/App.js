680f65bc025367b364425a1a48c9fa2f3cdf7d1d
Pankaj <pankajchadda282@gmail.com>
2023-10-18 18:37:22 +0530

    added navbar, home and checkout components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/navbar";
+import Home from "./pages/home/home";
+import Checkout from "./pages/checkout/checkout";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
