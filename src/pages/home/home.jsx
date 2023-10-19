c67c9265fa6933e1244bbcd63fa0f1bc89163e0b
Pankaj <pankajchadda282@gmail.com>
2023-10-19 17:24:59 +0530

    added assests(images),home page and its css.

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import { PRODUCTS } from "../../products";
+import { Product } from "./product";
+import "./home.css";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div class="home">
+      <div className="home-title">
+        <h1>GoBuy</h1>
+      </div>
+      <div className="products">
+        {PRODUCTS.map((product) => (
+          <Product data={product} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
